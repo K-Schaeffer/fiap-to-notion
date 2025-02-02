774fd449588a8091132054d271ff784fdd3bce04
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-02-02 03:19:27 -0300

    feat: initial authentication

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,35 @@
 import puppeteer from 'puppeteer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import { loginToFIAP } from './login';
+import { ensureAccessToCoursePage } from './courses';
 import path from 'path';
 
-(async () => {
-  const browser = await puppeteer.launch();
+async function main() {
+  const browser = await puppeteer.launch({ headless: false }); // Change to true to run in headless mode
   const page = await browser.newPage();
 
-  await page.goto('https://example.com');
+  if (!process.env.FIAP_USERNAME || !process.env.FIAP_PASSWORD) {
+    throw new Error('Please provide FIAP_USERNAME and FIAP_PASSWORD in .env file');
+  }
+
+  await loginToFIAP(page, {
+    username: process.env.FIAP_USERNAME,
+    password: process.env.FIAP_PASSWORD,
+  });
+
+  await ensureAccessToCoursePage(page);
+
   await page.screenshot({
-    path: path.join(process.cwd(), 'downloads/example.png') 
+    path: path.join(process.cwd(), 'downloads/coursesPage.png'),
   });
 
   await browser.close();
-})();
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
